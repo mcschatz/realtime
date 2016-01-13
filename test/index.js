6655ab43987734a40837af903552c7867edda316
Mimi Schatz <mcschatz@gmail.com>
2016-01-13 13:32:30 -0700

    removed game time started files, added server.js, and public dir

--- test/index.js
+++
@@ -1,7 +0,0 @@
-require('./point-test.js');
-require('./rectangle-test.js');
-require('./world-test.js');
-require('./entity-test.js');
-require('./player-test.js');
-require('./block-test.js');
-require('./squid-test.js');
