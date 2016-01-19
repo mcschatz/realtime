74393fc24a6ee628361397119d2b6054da72c707
Mimi Schatz <mcschatz@gmail.com>
2016-01-18 23:19:25 -0700

    admin page updated via websocket

--- public/client.js
+++ public/client.js
@@ -1,6 +1,23 @@
-var socket = io();
-var connectionCount = document.getElementById('connection-count');
+var adminPollId = window.location.pathname.match(/\/admin\/(.*)/);
 
-socket.on('usersConnected', function (count) {
-  connectionCount.innerText = 'Connected Users: ' + count;
+if (adminPollId) {
+  var socket = io({ query: 'adminPollId=' + adminPollId[1] });
+  socket.on('vote', function (data) {
+    var $vote = $('.admin-results ul li').filter(function () {
+      return $(this).data('key') === data
+    })
+    var voteCount = Number($vote.find('.tally').text());
+    voteCount += 1;
+    $vote.find('.tally').text(voteCount);
+  });
+}
+
+$('.option').on('click', function() {
+  var pollId = window.location.pathname.match(/\/poll\/(.*)/)[1];
+  $.ajax({
+    url: '/poll/' + pollId + '/vote',
+    method: 'POST',
+    data: { vote: $(this).text() }
+  });
 });
+
