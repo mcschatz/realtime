16468d3a8d3c419b986b90a3230ef6598d7ae717
Mimi Schatz <mcschatz@gmail.com>
2016-01-19 14:34:17 -0700

    database model tested

--- test/poll-test.js
+++ test/poll-test.js
@@ -6,47 +6,47 @@ const fixtures = require('./fixtures');
 
 describe('polls', function () {
   it('should instatiate a new poll', function () {
-    var poll = fixtures.validPoll;
+    var poll = new Poll(fixtures.validPoll);
     assert.isObject(poll);
   });
 
   it('should have options', function () {
-    var poll = fixtures.validPoll;
+    var poll = new Poll(fixtures.validPoll);
     assert.equal(poll.options[0], 'mushrooms');
   });
 
   it('should have a question', function () {
-    var poll = fixtures.validPoll;
+    var poll = new Poll(fixtures.validPoll);
     assert.equal(poll.question, 'What toppings do you want?');
   });
 
   it('should have an adminUrl', function () {
-    var poll = fixtures.validPoll;
-    assert.equal(poll.adminUrl, 1);
+    var poll = new Poll(fixtures.validPoll);
+    assert.equal(poll.adminUrl.length, 20);
   });
 
   it('should have a pollUrl', function () {
-    var poll = fixtures.validPoll;
-    assert.equal(poll.pollUrl, 2);
+    var poll = new Poll(fixtures.validPoll);
+    assert.equal(poll.pollUrl.length, 20);
   });
 
   it('should have votes', function () {
-    var poll = fixtures.validPoll;
-    assert.equal(poll.votes['black olives'], 3);
+    var poll = new Poll(fixtures.validPoll);
+    assert.equal(poll.votes['black olives'], 0);
   });
 
   it('should have a view status', function () {
-    var poll = fixtures.validPoll;
-    assert.equal(poll.publicView, 'true');
+    var poll = new Poll(fixtures.validPoll);
+    assert.equal(poll.publicView, undefined);
   });
 
   it('should have status', function () {
-    var poll = fixtures.validPoll;
+    var poll = new Poll(fixtures.validPoll);
     assert.equal(poll.status, 'Open');
   });
 
-  it('could have a setTimeout', function () {
-    var poll = fixtures.validPoll;
-    assert.equal(poll.setTimeout, 5000);
+  it('should have a setTimeout', function () {
+    var poll = new Poll(fixtures.validPoll);
+    assert.equal(poll.setTimeout, 300000000);
   });
 });
